2f0175ee80df0ccee51262844f4d906dc06bd586
Spencer Dupre <spencer.dupre@gmail.com>
2019-03-05 20:33:04 -0500

    balance fidelity test adds phony funds to main account for testing, if needed

--- tests/test_balance_fidelity.php
+++ tests/test_balance_fidelity.php
@@ -5,9 +5,13 @@ require_once('utils.php');
 
 $client = new Client($rpc_host, $rpc_port, $rpc_user, $rpc_pass);
 
+if ($client->getBalance($hot_account_main) == 0)
+    $client->jsonrpc->move('', "zelles($hot_account_main)", 100);
+
 $accounts = $client->jsonrpc->listaccounts();
 foreach ($accounts as $acc => $bal) {
-    my_assert($bal >= 0, '', "$acc balance not >= 0, actually $bal");
+    if ($acc)
+        my_assert($bal >= 0, '', "$acc balance ($bal) not >= 0");
 }
 
 // Test initial hot balances
@@ -24,5 +28,6 @@ $wait_bal = floatval($client->getBalance($hot_account_wait));
 my_assert($wait_bal > 0, '', 'Wait balance <= 0');
 $diff_bal = $main_bal - floatval($client->getBalance($hot_account_main));
 my_assert($diff_bal > 0, '', 'Main balance is the same as before');
-my_assert($wait_bal == $diff_bal, '', 'Difference between wait and diff balances');
+my_assert($wait_bal == $diff_bal, '', 'wait and diff balances !=');
+
 ?>
